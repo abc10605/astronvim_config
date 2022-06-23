c3af20a1c8e002009530483ee7ba64d4970c4a40
abc10605 <chenghsuanho@gmail.com>
2022-06-23 15:50:10 +0800

    feat(plugins): add todo-comment plugin

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,16 +1,21 @@
 -- Add plugins, the packer syntax without the "use"
 return {
-  -- You can disable default plugins as follows:
-  -- ["goolord/alpha-nvim"] = { disable = true },
+	-- You can disable default plugins as follows:
+	-- ["goolord/alpha-nvim"] = { disable = true },
 
-  -- You can also add new plugins here as well:
-  -- { "andweeb/presence.nvim" },
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-  { "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" },
+	-- You can also add new plugins here as well:
+	-- { "andweeb/presence.nvim" },
+	-- {
+	--   "ray-x/lsp_signature.nvim",
+	--   event = "BufRead",
+	--   config = function()
+	--     require("lsp_signature").setup()
+	--   end,
+	-- },
+
+	-- todo comments
+	{
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = require("todo-comments").setup(),
 }
