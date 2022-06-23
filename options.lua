9c16c25810bf40670a4546f6cfe143d13516c87b
abc10605 <chenghsuanho@gmail.com>
2022-06-23 15:39:18 +0800

    refactor(options): disable fold for now

--- options.lua
+++ options.lua
@@ -1,12 +1,9 @@
 return {
-  opt = {
-    relativenumber = false, -- sets vim.opt.relativenumber
-    -- colorcolumn = "80",
-    foldenable = false,
-    foldexpr = "nvim_treesitter#foldexpr()", -- set Treesitter based folding
-    foldmethod = "expr"
-  },
-  g = {
-    mapleader = " ", -- sets vim.g.mapleader
-  },
+	opt = {
+		relativenumber = false, -- sets vim.opt.relativenumber
+		-- colorcolumn = "80",
+	},
+	g = {
+		mapleader = " ", -- sets vim.g.mapleader
+	},
 }
