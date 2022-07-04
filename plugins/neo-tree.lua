3239503a572fd6c297737e6fb4179790af1cb6f3
abc10605 <chenghsuanho@gmail.com>
2022-07-04 15:25:00 +0800

    feat(neo-tree): copy file name and path

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -2,4 +2,26 @@ return {
 	close_if_last_window = true,
 	enable_diagnostics = true,
 	enable_git_status = true,
+	filesystem = {
+		commands = {
+			copy_file_name = function(state)
+				local node = state.tree:get_node()
+				vim.fn.setreg("*", node.name, "c")
+				vim.notify({ "Copied file's name:", node.name }, "INFO", { title = "neo-tree" })
+			end,
+			copy_relative_path = function(state)
+				local node = state.tree:get_node()
+				local full_path = node.path
+				local relative_path = full_path:sub(#state.path + 2)
+				vim.fn.setreg("*", relative_path, "c")
+				vim.notify({ "Copied file's relative path:", relative_path }, "INFO", { title = "neo-tree" })
+			end,
+		},
+		window = {
+			mappings = {
+				["Y"] = "copy_file_name",
+				["<C-y>"] = "copy_relative_path",
+			},
+		},
+	},
 }
