02865810adbd4f311bed5d3ffe5597b21f419972
Ho Cheng Hsuan <chenghsuanho@gmail.com>
2022-10-24 22:45:16 +0800

    refactor(cmp): add back priority

--- cmp.lua
+++ cmp.lua
@@ -31,4 +31,14 @@ return {
 			end
 		end, { "i", "s" }),
 	},
+	source_priority = {
+		nvim_lsp = 1000,
+		omni = 800,
+		luasnip = 750,
+		emoji = 700,
+		pandoc_references = 700,
+		calc = 650,
+		path = 500,
+		buffer = 250,
+	},
 }
